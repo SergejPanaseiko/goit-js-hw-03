79af274d37d883cc55a2e8b545952b452668514c
Sergey <konsaltpns@gmail.com>
2024-12-01 16:08:36 +0200

    add home work

--- js/task-1.js
+++ js/task-1.js
@@ -5,9 +5,9 @@ function slugify(title) {
     return title;
 }
 
-console.log("------------- TASK 1 -------------------")
+console.log("------------- TASK 1 -------------------");
 console.log(slugify("Arrays for beginners"));
 console.log(slugify("English for developer"));
 console.log(slugify("Ten secrets of JavaScript"));
 console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
-console.log(" ")
+console.log(" ");
--- js/task-2.js
+++ js/task-2.js
@@ -4,11 +4,12 @@ function makeArray(firstArray, secondArray, maxLength)
     return arrayNew.slice(0,maxLength);
 }
 
-console.log("------------- TASK 2 -------------------")
+
+console.log("------------- TASK 2 -------------------");
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
 console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
-console.log(" ")
+console.log(" ");
